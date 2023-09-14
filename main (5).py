e74936f6821bc43d0751c6728cc59fdb85665f72
Mvanifatyev2 <136022351+Mvanifatyev2@users.noreply.github.com>
2023-09-14 08:15:22 +0300

    sem 7

---
+++ main (5).py
@@ -0,0 +1,15 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+
+print_operation_table(lambda x, y: x * y)
